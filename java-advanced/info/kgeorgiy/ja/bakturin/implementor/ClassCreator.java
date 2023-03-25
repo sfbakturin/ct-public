8ebbe2fc7956b5dd118758a01749c58251a79e19
Saveliy Bakturin <sfbakturin@mail.ru>
2023-03-25 12:02:23 +0300

    [java-advanced.implementor] `Jar Implementor` and `Javadoc` homework upload.

--- java-advanced/info/kgeorgiy/ja/bakturin/implementor/ClassCreator.java
+++ java-advanced/info/kgeorgiy/ja/bakturin/implementor/ClassCreator.java
@@ -13,44 +13,115 @@ import java.util.stream.Collectors;
  * Don't write off, if you don't wanna be banned!
  */
 
+/**
+ * The {@code Class<?>} that generates the subject interface.
+ */
 public class ClassCreator {
+	/**
+	 * Constant value of line separator.
+	 * Used in class generator to divide by newlines for human-readable text formatting of Java source code.
+	 */
 	private static final String LINE_SEPARATOR = System.lineSeparator();
+	/**
+	 * Constant value for formatting {@code package} name.
+	 * Used in the class generator to correctly form a string containing information about the {@code package} in which the source code of the class is located, if the supplied {@code Class<?>} really lies in some non-root {@code package}.
+	 */
 	private static final String FORMAT_PACKAGE = "package %s;";
+	/**
+	 * Constant value for formatting {@code Class<?>} declaration of the generated class.
+	 */
 	private static final String FORMAT_CLASS = "public class %sImpl implements %s";
+	/**
+	 * Constant value for formatting method's declaration of the generated class.
+	 */
 	private static final String FORMAT_METHOD = "public %s %s(%s)";
+	/**
+	 * Constant value for formatting a string used as an enumeration point for {@code Exception} types from a method of the generated class.
+	 */
 	private static final String FORMAT_THROWS = "throws %s";
+	/**
+	 * Constant value for formatting a string used as a return point from a method of the generated class.
+	 */
 	private static final String FORMAT_RETURN = "return %s;";
+	/**
+	 * Constant value for initiation of any constructs of the generated class.
+	 */
 	private static final String FORMAT_BEGIN = " " + "{" + LINE_SEPARATOR;
+	/**
+	 * Constant value for completion of any constructs of the generated class.
+	 */
 	private static final String FORMAT_END = "}" + LINE_SEPARATOR;
-	private static final String FORMAT_NEXT = ",";
+	/**
+	 * Constant value for formatting sequence of objects in {@code Parameter} pack or exceptions.
+	 */
+	private static final String FORMAT_NEXT = "," + " ";
+	/**
+	 * Constant value of tabulation.
+	 */
 	private static final String FORMAT_TABULATION = "\t";
+	/**
+	 * Constant value of string meaning overriding methods from {@code Interface}.
+	 */
+	private static final String FORMAT_OVERRIDE = "@Override";
+	/**
+	 * The buffer value of generalized {@code Class<?>}.
+	 * Used for buffered storage of the current state of the generated class in the form of mandatory human-readable formatted Java source code.
+	 *
+	 * @see StringBuilder
+	 */
 	private final StringBuilder sb;
-	private final Class<?> token;
+	/**
+	 * The reference value of {@code Class<?>} that is being generated.
+	 * Used to access the field from outside the generator without copying the token itself.
+	 *
+	 * @see Class
+	 */
+	private final Class<?> tok;
 
+	/**
+	 * Construct of {@code ClassCreator} from reference of {@code Class<?>}.
+	 *
+	 * @param token {@code Class<?>}, a reference to {@code Class<?>} that is being generated.
+	 */
 	public ClassCreator(final Class<?> token) {
 		sb = new StringBuilder();
-		this.token = token;
+		tok = token;
 	}
 
+	/**
+	 * Main function of generating {@code Class<?>} from specified {@code Interface}.
+	 */
 	public void genClass() {
-		writePackage(token.getPackage());
+		writePackage(tok.getPackage());
 		writeNewLine();
 		writeNewLine();
-		writeName(token);
+		writeName(tok);
 		writeBegin();
 		writeNewLine();
-		for (final Method item : token.getMethods()) {
+		for (final Method item : tok.getMethods()) {
 			writeMethod(item);
 		}
 		writeEnd();
 		writeNewLine();
 	}
 
+	/**
+	 * Return human-readable {@code String} of generated specified {@code Class<?>}.
+	 *
+	 * @return generated and formatted {@code String} from {@code StringBuilder}.
+	 * @see StringBuilder#toString()
+	 */
 	@Override
 	public String toString() {
 		return sb.toString();
 	}
 
+	/**
+	 * {@code Method} for writing the {@code package} of the generated {@code Class<?>}.
+	 *
+	 * @param name {@code Package} that should be written to private {@code StringBuilder} for buffer.
+	 * @see Package#getName()
+	 */
 	private void writePackage(final Package name) {
 		final String packageName = name.getName();
 		if (packageName.length() != 0) {
@@ -58,22 +129,54 @@ public class ClassCreator {
 		}
 	}
 
+	/**
+	 * {@code Method} for writing the signature of the generated {@code Class<?>}.
+	 *
+	 * @param name {@code Class<?>}, that should be used to get real name of generated {@code Class<?>} and real name of implemented {@code Interface}.
+	 * @see Class#getSimpleName()
+	 * @see Class#getCanonicalName()
+	 */
 	private void writeName(final Class<?> name) {
 		sb.append(String.format(FORMAT_CLASS, name.getSimpleName(), name.getCanonicalName()));
 	}
 
+	/**
+	 * An abstract {@code Method} for formatting a given getter into a list with commas.
+	 *
+	 * @param ts {@code <T>[]} objects from which to format into a string.
+	 * @param getter {@code Function<? super T, ? extends String>} a function to get a string from the supplied objects.
+	 * @return {@code String} reformatted by getter and FORMAT_NEXT.
+	 * @param <T> {@code <T>} type of transferred objects.
+	 */
 	private static <T> String format(final T[] ts, final Function<? super T, ? extends String> getter) {
 		return Arrays.stream(ts).map(getter).collect(Collectors.joining(FORMAT_NEXT));
 	}
 
+	/**
+	 * {@code Method} for formatting the list of parameters passed to the method.
+	 *
+	 * @param parameters {@code Parameter[]} parameters to be formatted on a single line.
+	 * @return {@code String} formatting result.
+	 */
 	private static String formatMethodParameters(final Parameter[] parameters) {
 		return format(parameters, s -> s.getType().getCanonicalName() + " " + s.getName());
 	}
 
+	/**
+	 * {@code Method} for formatting the list of method exceptions thrown.
+	 *
+	 * @param exceptions {@code Class<?>[]} exception types to be formatted on a single line.
+	 * @return {@code String} formatting result.
+	 */
 	private static String formatMethodExceptions(final Class<?>[] exceptions) {
 		return format(exceptions, Class::getCanonicalName);
 	}
 
+	/**
+	 * {@code Method} for writing the signature of the given method in the generated {@code Class<?>}.
+	 *
+	 * @param method {@code Method} from which we get the signature of this method.
+	 */
 	private void writeMethodHead(final Method method) {
 		final Class<?>[] exceptions = method.getExceptionTypes();
 		final Parameter[] parameters = method.getParameters();
@@ -83,6 +186,11 @@ public class ClassCreator {
 		}
 	}
 
+	/**
+	 * {@code Method} for writing a default return value in the generated {@code Class<?>}.
+	 *
+	 * @param method {@code Method} from which we get the default value of the return type.
+	 */
 	private void writeMethodReturn(final Method method) {
 		if (method.getReturnType() != Void.TYPE) {
 			final String type = (method.getReturnType().isPrimitive() ? (method.getReturnType() == Boolean.TYPE ? "false" : "0") : "null");
@@ -90,10 +198,18 @@ public class ClassCreator {
 		}
 	}
 
+	/**
+	 * {@code Method} that is a sequence of other methods to generate the given method in the generated {@code Class<?>}.
+	 *
+	 * @param method {@code Method} method that will be implemented at the moment.
+	 */
 	private void writeMethod(final Method method) {
 		if (method.isDefault() || Modifier.isStatic(method.getModifiers())) {
 			return;
 		}
+		writeTab();
+		writeOverride();
+		writeNewLine();
 		writeTab();
 		writeMethodHead(method);
 		writeBegin();
@@ -106,19 +222,38 @@ public class ClassCreator {
 		writeNewLine();
 	}
 
+	/**
+	 * {@code Method} for writing a new line in the generated {@code Class<?>}.
+	 */
 	private void writeNewLine() {
 		sb.append(LINE_SEPARATOR);
 	}
 
+	/**
+	 * {@code Method} for writing a tab in the generated {@code Class<?>}.
+	 */
 	private void writeTab() {
 		sb.append(FORMAT_TABULATION);
 	}
 
+	/**
+	 * {@code Method} for writing a curly brace, denoting the start of a construct of a generated {@code Class<?>}.
+	 */
 	private void writeBegin() {
 		sb.append(FORMAT_BEGIN);
 	}
 
+	/**
+	 * {@code Method} for writing a curly brace that marks the end of a construct generated by a {@code Class<?>}.
+	 */
 	private void writeEnd() {
 		sb.append(FORMAT_END);
+	}
+
+	/**
+	 * {@code Method} for writing the inscription {@code @Override} of the generated {@code Class<?>}.
+	 */
+	private void writeOverride() {
+		sb.append(FORMAT_OVERRIDE);
 	}
 }
--- java-advanced/info/kgeorgiy/ja/bakturin/implementor/Implementor.java
+++ java-advanced/info/kgeorgiy/ja/bakturin/implementor/Implementor.java
@@ -2,11 +2,16 @@ package info.kgeorgiy.ja.bakturin.implementor;
 
 import info.kgeorgiy.java.advanced.implementor.Impler;
 import info.kgeorgiy.java.advanced.implementor.ImplerException;
+import info.kgeorgiy.java.advanced.implementor.JarImpler;
 
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.Modifier;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
@@ -14,7 +19,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.jar.Attributes;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+import java.util.zip.ZipEntry;
 
 /**
  * @author Saveliy Bakturin
@@ -22,33 +30,162 @@ import java.util.stream.Collectors;
  * Don't write off, if you don't wanna be banned!
  */
 
-public class Implementor implements Impler {
+/**
+ * The main {@code Class<?>} for passing {@code Interface} tests.
+ */
+public class Implementor implements Impler, JarImpler {
+	/**
+	 * A {@code static} method that returns the actual pure name of the {@code Class<?>}.
+	 * The real pure name is a sequence of strings that is the full {@code package} name and, as the last element, the class name without unnecessary nesting. The last element is simple name from {@code Class<?>}.
+	 *
+	 * @param token {@code Class<?>} from which we want the pure real name.
+	 * @return {@code String[]} - an array whose elements are strings in the correct order for the real pure name of the {@code Class<?>}.
+	 * @see Class#getPackage()
+	 * @see Package#getName()
+	 * @see Class#getSimpleName()
+	 */
+	private static String[] getFullPureName(final Class<?> token) {
+		final String packageName = token.getPackage().getName();
+		final String pureClassName = token.getSimpleName() + "Impl";
+		final String className = (packageName.length() == 0 ? pureClassName : packageName + "." + pureClassName);
+		return className.split("\\.");
+	}
+
+	/**
+	 * A {@code static} method that is used to get the relative path to a {@code Class<?>} using its pure real name.
+	 *
+	 * @param token {@code Class<?>} - from which we want the pure real name and generate file system path.
+	 * @param root  {@code String} - in the event that the {@code Class<?>} lies in some directory, then the root directory should be submitted.
+	 * @return {@code String} - received path to the file as a {@code String}.
+	 * @see Implementor#getFullPureName(Class)
+	 */
+	private static String getFileName(final Class<?> token, final String root) {
+		final String name = String.join(File.separator, getFullPureName(token));
+		if (root.length() == 0) {
+			return name;
+		} else {
+			return root + File.separator + name;
+		}
+	}
+
+	/**
+	 * A {@code static} method that is used to get the relative path to a {@code Class<?>} using its pure real name and java-extension.
+	 *
+	 * @param token {@code Class<?>} - from which we want the pure real name and generate file system path with java-extension.
+	 * @param root  {@code String} in the event that the {@code Class<?>} lies in some directory, then the root directory should be submitted.
+	 * @return {@code String} - received path to the file as a {@code String}.
+	 * @see Implementor#getFileName(Class, String)
+	 */
+	private static String getFileNameJava(final Class<?> token, final String root) {
+		return getFileName(token, root) + ".java";
+	}
+
+	/**
+	 * A {@code static} method that is used to get the relative path to a {@code Class<?>} using its pure real name and class-extension.
+	 *
+	 * @param token {@code Class<?>} from which we want the pure real name and generate file system path with class-extension.
+	 * @param root  {@code String} in the event that the {@code Class<?>} lies in some directory, then the root directory should be submitted.
+	 * @return {@code String} received path to the file as a {@code String}.
+	 * @see Implementor#getFileName(Class, String)
+	 */
+	private static String getFileNameClass(final Class<?> token, final String root) {
+		return getFileName(token, root) + ".class";
+	}
+
+	/**
+	 * A {@code static} method that is a wrapper over {@code Implementor#getFileNameJava(Class, String)}, which takes a {@code Path} instead of a {@code String}.
+	 *
+	 * @param token {@code Class<?>} from which we want the pure real name and generate file system path with java-extension.
+	 * @param root  {@code Path} in the event that the {@code Class<?>} lies in some directory, then the root directory should be submitted.
+	 * @return {@code String} received path to the file as a {@code String}.
+	 * @see Implementor#getFileNameJava(Class, String)
+	 */
+	private static String getFileNameJava(final Class<?> token, final Path root) {
+		return getFileNameJava(token, root.toString());
+	}
+
+	/**
+	 * A {@code static} method that is a wrapper over {@code Implementor#getFileNameClass(Class, String)}, which takes a {@code Path} instead of a {@code String}.
+	 *
+	 * @param token {@code Class<?>} from which we want the pure real name and generate file system path with class-extension.
+	 * @param root  {@code Path} in the event that the {@code Class<?>} lies in some directory, then the root directory should be submitted.
+	 * @return {@code String} received path to the file as a {@code String}.
+	 * @see Implementor#getFileNameJava(Class, String)
+	 */
+	private static String getFileNameClass(final Class<?> token, final Path root) {
+		return getFileNameClass(token, root.toString());
+	}
+
+	/**
+	 * A {@code static} method for compiling files.
+	 *
+	 * @param token {@code Class<?>} is used to get the absolute path to the compiled file.
+	 * @param root  {@code Path} is used to get the root folder before the folder where the file is compiled.
+	 * @param file  {@code String} the full path to the Java source code to be compiled.
+	 * @throws ImplerException used when no compiler is found or the compilation arguments are invalid.
+	 */
+	private static void compileFile(final Class<?> token, final Path root, final String file) throws ImplerException {
+		final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		if (Objects.isNull(compiler)) {
+			throw new ImplerException("Implementing failed due to java compiler not found.");
+		}
+		final String classpath = root + File.pathSeparator + getClassPath(token);
+		final String[] args = new String[]{file, "-cp", classpath};
+		try {
+			compiler.run(null, null, null, args);
+		} catch (final NullPointerException e) {
+			throw new ImplerException("Implementing failed due to null as arguments for compiling.", e);
+		}
+	}
+
+	/**
+	 * A {@code static} method that returns the full absolute path to the given {@code Class<?>}.
+	 *
+	 * @param token {@code Class<?>} used as the main argument to get an absolute path.
+	 * @return {@code String} the full absolute path to the {@code Class<?>}.
+	 * @throws ImplerException in case of an {@code URISyntaxException}.
+	 */
+	private static String getClassPath(final Class<?> token) throws ImplerException {
+		try {
+			return Path.of(token.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
+		} catch (final URISyntaxException e) {
+			throw new ImplerException("Implementing failed due to URI syntax error.", e);
+		}
+	}
+
+	/**
+	 * A {@code static} method for generating the base manifest of a Jar file.
+	 *
+	 * @return {@code Manifest} generated base manifest.
+	 */
+	private static Manifest getManifest() {
+		final Manifest man = new Manifest();
+		man.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+		return man;
+	}
 
 	@Override
 	public void implement(final Class<?> token, final Path root) throws ImplerException {
 		if (!token.isInterface()) {
-			throw new ImplerException("Class<?> token should be interface.");
+			throw new ImplerException("Implementing failed due to class<?> token should be interface.");
 		}
 
 		if (Modifier.isPrivate(token.getModifiers())) {
-			throw new ImplerException("Interface token should be not private.");
+			throw new ImplerException("Implementing failed due to interface token should be not private.");
 		}
 
-		final String packageName = token.getPackage().getName();
-		final String pureClassName = token.getSimpleName() + "Impl";
-		final String className = (packageName.length() == 0 ? pureClassName : packageName + "." + pureClassName);
-		final String directoryName = root.toString() + Arrays.stream(className.split("\\.")).map(s -> File.separator + s).collect(Collectors.joining());
-		final String fileName = directoryName + ".java";
+		final String fileName = getFileNameJava(token, root);
 
 		try {
-			final Path directoryPath = Paths.get(directoryName).getParent();
 			final Path classPath = Paths.get(fileName);
+			final Path directoryPath = classPath.getParent();
 
 			try {
 				if (directoryPath != null) {
 					Files.createDirectories(directoryPath);
 				}
 			} catch (final IOException ignored) {
+				System.err.println("Failed to create directories.");
 			}
 
 			try (final BufferedWriter writer = Files.newBufferedWriter(classPath, StandardCharsets.UTF_8)) {
@@ -56,35 +193,64 @@ public class Implementor implements Impler {
 				creator.genClass();
 				writer.write(creator.toString());
 			} catch (final IOException e) {
-				throw new ImplerException(e.getMessage());
+				throw new ImplerException("Implementing failed due to writer error.", e);
 			}
 		} catch (final InvalidPathException e) {
-			throw new ImplerException(e.getMessage());
+			throw new ImplerException("Implementing failed due to invalid path.", e);
 		}
 	}
 
-	public static void main(final String[] args) throws ImplerException {
+	@Override
+	public void implementJar(final Class<?> token, final Path jarFile) throws ImplerException {
+		final Path root = jarFile.getParent();
+		implement(token, root);
+		final String fileJarName = getFileNameClass(token, "");
+		final String fileName = getFileNameClass(token, root);
+		compileFile(token, root, getFileNameJava(token, root));
+		try (final OutputStream os = Files.newOutputStream(jarFile)) {
+			try (final JarOutputStream jos = new JarOutputStream(os, getManifest())) {
+				final ZipEntry ze = new ZipEntry(fileJarName.replace(File.separatorChar, '/'));
+				jos.putNextEntry(ze);
+				Files.copy(Path.of(fileName), jos);
+				jos.closeEntry();
+			} catch (final NullPointerException | IllegalArgumentException | IOException | SecurityException e) {
+				throw new ImplerException("Implementing failed due to broken file.", e);
+			}
+		} catch (final IOException e) {
+			throw new ImplerException("Implementing failed due to writer error.", e);
+		}
+	}
+
+	/**
+	 * Main {@code Method}.
+	 *
+	 * @param args {@code String[]} arguments of the entry point of the main part of the program.
+	 */
+	public static void main(final String[] args) {
 		if (Objects.isNull(args)) {
 			System.err.println("Arguments array is null.");
 			return;
 		}
 
-		if (args.length != 2) {
-			System.err.println("Usage of class is <class> <interface> <path>");
-			return;
-		}
-
-		if (args[0] == null || args[1] == null) {
+		if (Arrays.stream(args).anyMatch(Objects::isNull)) {
 			System.err.println("Arguments array is invalid");
 			return;
 		}
 
 		try {
-			new Implementor().implement(Class.forName(args[0]), Paths.get(args[1]));
-		} catch (final InvalidPathException e) {
-			System.err.println("Invalid path: " + e.getMessage());
+			if (args.length == 2) {
+				new Implementor().implement(Class.forName(args[0]), Paths.get(args[1]));
+			} else if (args.length == 3 && args[0].equals("-jar") && args[2].endsWith(".jar")) {
+				new Implementor().implementJar(Class.forName(args[1]), Paths.get(args[2]));
+			} else {
+				System.err.println("Invalid number of arguments or invalid arguments");
+			}
 		} catch (final ClassNotFoundException e) {
-			System.err.println("Interface to implement is not found: " + e.getMessage());
+			System.err.println("Class not found: " + e.getMessage());
+		} catch (final InvalidPathException e) {
+			System.err.println("Invalid path was given: " + e.getMessage());
+		} catch (final ImplerException e) {
+			System.err.println("Failed: " + e.getMessage());
 		}
 	}
 }
