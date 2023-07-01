f1d457f6a7be275f29015396ea0a1436bf42e467
Saveliy Bakturin <sfbakturin@mail.ru>
2023-07-01 15:38:05 +0300

    [algorithms.half] Refactoring to new task's desc.

---
+++ algorithms/half/O/O.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * @author Saveliy Bakturin
+ * <p>
+ * Don't write off, if you don't wanna be banned!
+ */
+
+public class O {
+	private static double f(final double i) {
+		return Math.pow(i, 2) + Math.sqrt(i);
+	}
+
+	public static void main(final String... args) {
+		final Scanner in = new Scanner(System.in);
+		final double c = in.nextDouble();
+		in.close();
+		double left = 0;
+		double right = Math.pow(10, 10);
+		final double eps = 0.0000001;
+		while (right - left > eps) {
+			double mid = (right + left) / 2;
+			if (f(mid) <= c) {
+				left = mid;
+			} else {
+				right = mid;
+			}
+		}
+		System.out.println(left);
+	}
+}
