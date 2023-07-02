6cb644ab8c76aa5a33642b0c11467f2fd71442c1
Saveliy Bakturin <sfbakturin@mail.ru>
2023-07-02 16:20:22 +0300

    [discrete-math.encoding] Refactoring task's desc.

---
+++ discrete-math/encoding/D/D.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Saveliy Bakturin
+ * <p>
+ * Don't write off, if you don't wanna be banned!
+ */
+
+public class D {
+	public static void main(final String... args) {
+		final Scanner in = new Scanner(System.in);
+		final String s = in.nextLine();
+		in.close();
+		final int[] alphabet = new int[256];
+		for (int i = 0; i < 26; i++) {
+			alphabet[i + 97] = i + 1;
+		}
+		int index = -1;
+		final List<Integer> answer = new ArrayList<>();
+		mtf(s, alphabet, index, answer);
+		for (final Integer integer : answer) {
+			System.out.print(integer + " ");
+		}
+	}
+
+	private static void mtf(final String s, final int[] alphabet, int index, final List<Integer> answer) {
+		for (int i = 0; i < s.length(); i++) {
+			answer.add(alphabet[s.charAt(i)]);
+			if (alphabet[s.charAt(i)] != 1) {
+				for (int j = s.charAt(i) - 1; j >= 97; j--) {
+					if (alphabet[s.charAt(i)] > alphabet[j]) {
+						alphabet[j]++;
+					}
+				}
+				for (int j = s.charAt(i) + 1; j <= 122; j++) {
+					if (alphabet[s.charAt(i)] > alphabet[j]) {
+						alphabet[j]++;
+					}
+				}
+				if (index != -1) {
+					alphabet[index] = 2;
+				}
+				alphabet[s.charAt(i)] = 1;
+				index = s.charAt(i);
+			}
+		}
+	}
+}
---
+++ discrete-math/encoding/G/G.py
@@ -0,0 +1,83 @@
+import math
+from decimal import *
+
+#
+# @author Saveliy Bakturin
+# <p>
+# Don't write off, if you don't wanna be banned!
+#
+
+getcontext().prec = 200
+n = int(input())
+s = input()
+count = [0] * 256
+alphabet = set(s)
+maxcharacter = chr(97)
+
+
+def fill_alphabet():
+	global x, maxcharacter
+	for x in range(len(s)):
+		count[ord(s[x])] += 1
+		alphabet.add(s[x])
+		maxcharacter = chr(max(ord(maxcharacter), ord(s[x])))
+
+
+fill_alphabet()
+m = len(alphabet)
+p, pl, pr, left_temp = [0] * 256, [0] * 256, [0] * 256, 0
+
+
+def fill_left_right():
+	global x, left_temp
+	for x in range(ord(maxcharacter) - 97 + 1):
+		p[97 + x] = Decimal(count[97 + x]) / Decimal(len(s))
+		pl[97 + x] = left_temp
+		pr[97 + x] = left_temp + p[(97 + x)]
+		left_temp = pr[(97 + x)]
+
+
+fill_left_right()
+left, right = Decimal(0), Decimal(1)
+
+
+def decode():
+	global x, left, right
+	for x in range(len(s)):
+		r = Decimal(left) + Decimal(right - left) * Decimal(pr[ord(s[x])])
+		l = Decimal(left) + Decimal(right - left) * Decimal(pl[ord(s[x])])
+		left = l
+		right = r
+
+
+decode()
+q, p = 1001, 0
+loop = True
+
+for x in range(1, 10000000000000):
+	if not loop:
+		break
+	for y in range(math.floor(left * (1 << x)), 1 << x):
+		if left <= Decimal(y) / Decimal(1 << x) < right:
+			q = x
+			p = y
+			loop = False
+			break
+		if Decimal(y) / Decimal(1 << x) >= right:
+			break
+sbin_p = bin(p)[2:]
+u = 0
+
+
+def answer_print():
+	global x, sbin_p
+	print(n)
+	for x in range(97, ord(maxcharacter) + 1):
+		print(count[x], end=" ")
+	print()
+	while len(sbin_p) != q:
+		sbin_p = "0" + sbin_p
+	print(sbin_p)
+
+
+answer_print()
